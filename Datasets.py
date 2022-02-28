a5faf067b7baa6e48fea94e85175bde8e20230a2
NilsLusch <96436846+NilsLusch@users.noreply.github.com>
2022-02-28 20:07:22 +0100

    Create Datasets.py

---
+++ Datasets.py
@@ -0,0 +1,127 @@
+"""
+@Author: Yue Wang, partly revised by Shi Qiu (shi.qiu@anu.edu.au)
+@Contact: yuewangx@mit.edu
+@File: data.py
+@Time: 2018/10/13 6:21 PM
+"""
+
+
+import os
+import sys
+import glob
+import h5py
+import numpy as np
+from torch.utils.data import Dataset
+
+
+def download():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
+        www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
+        zipfile = os.path.basename(www)
+        os.system('wget %s; unzip %s' % (www, zipfile))
+        os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
+        os.system('rm %s' % (zipfile))
+
+
+def load_data(partition):
+    download()
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    all_data = []
+    all_label = []
+    for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
+        f = h5py.File(h5_name)
+        data = f['data'][:].astype('float32')
+        label = f['label'][:].astype('int64')
+        f.close()
+        all_data.append(data)
+        all_label.append(label)
+    all_data = np.concatenate(all_data, axis=0)
+    all_label = np.concatenate(all_label, axis=0)
+    return all_data, all_label
+
+
+def load_scanobjectnn_data(partition):
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    all_data = []
+    all_label = []
+
+    h5_name = BASE_DIR + '/data/' + partition + '_objectdataset_augmentedrot_scale75.h5'
+    f = h5py.File(h5_name)
+    data = f['data'][:].astype('float32')
+    label = f['label'][:].astype('int64')
+    f.close()
+    all_data.append(data)
+    all_label.append(label)
+    all_data = np.concatenate(all_data, axis=0)
+    all_label = np.concatenate(all_label, axis=0)
+    return all_data, all_label
+
+
+def translate_pointcloud(pointcloud):
+    xyz1 = np.random.uniform(low=2./3., high=3./2., size=[3])
+    xyz2 = np.random.uniform(low=-0.2, high=0.2, size=[3])
+       
+    translated_pointcloud = np.add(np.multiply(pointcloud, xyz1), xyz2).astype('float32')
+    return translated_pointcloud
+
+
+def jitter_pointcloud(pointcloud, sigma=0.01, clip=0.02):
+    N, C = pointcloud.shape
+    pointcloud += np.clip(sigma * np.random.randn(N, C), -1*clip, clip)
+    return pointcloud
+
+#Source: https://github.com/Strawberry-Eat-Mango/PCT_Pytorch/blob/main/data.py
+def random_point_dropout(pointcloud, max_dropout_ratio=0.875):
+    ''' batch_pc: BxNx3 '''
+    # for b in range(batch_pc.shape[0]):
+    dropout_ratio = np.random.random()*max_dropout_ratio # 0~0.875    
+    drop_idx = np.where(np.random.random((pointcloud.shape[0]))<=dropout_ratio)[0]
+    # print ('use random drop', len(drop_idx))
+
+    if len(drop_idx)>0:
+        pointcloud[drop_idx,:] = pointcloud[0,:] # set to the first point
+    return pointcloud
+
+class ModelNet40(Dataset):
+    def __init__(self, num_points, partition='train', data_augmentation=False):
+        self.data, self.label = load_data(partition)
+        self.num_points = num_points
+        self.partition = partition
+        self.data_augmentation = data_augmentation
+
+    def __getitem__(self, item):
+        pointcloud = self.data[item][:self.num_points]
+        label = self.label[item]
+        if self.partition == 'train':
+            if self.data_augmentation == True:
+                pointcloud = translate_pointcloud(pointcloud)
+                pointcloud = random_point_dropout(pointcloud)
+            np.random.shuffle(pointcloud)
+        return pointcloud, label
+
+    def __len__(self):
+        return self.data.shape[0]
+
+
+class ScanObjectNN(Dataset):
+    def __init__(self, num_points, partition='training'):
+        self.data, self.label = load_scanobjectnn_data(partition)
+        self.num_points = num_points
+        self.partition = partition        
+
+    def __getitem__(self, item):
+        pointcloud = self.data[item][:self.num_points]
+        label = self.label[item]
+        if self.partition == 'training':
+            np.random.shuffle(pointcloud)
+            pointcloud = translate_pointcloud(pointcloud)
+        return pointcloud, label
+
+    def __len__(self):
+        return self.data.shape[0]
