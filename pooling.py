b5d97bcbd7869b6405eb0611c4fb81c2d0c327f2
NilsLusch <96436846+NilsLusch@users.noreply.github.com>
2022-02-28 21:10:19 +0100

    Update pooling.py

--- pooling.py
+++ pooling.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from pytorch3d.ops import knn_points,knn_gather, sample_farthest_points
 
 class MA_Pooling(nn.Module):
     def __init__(self, pooling):
@@ -17,4 +18,18 @@ class MA_Pooling(nn.Module):
                     x = avg_pool
         if self.pooling == "both":
             x = torch.cat((max_pool, avg_pool), dim=1)
+        return x
+
+class KNN_Pooling(nn.Module):
+    def __init__(self, sample, k):
+        super(KNN_Pooling, self).__init__()
+        self.sample = int(sample) 
+        self.k = int(k)
+    def forward(self, x, coordinates):
+        x = x.permute(0,2,1)
+        sample_coords, sample_idx = sample_farthest_points(coordinates, K=self.sample)
+        _,knn_idx,_ = knn_points(sample_coords, coordinates, K=self.k)
+        x  = knn_gather(x, knn_idx)
+        x = torch.max(x,2)[0]
+        x = torch.flatten(x, start_dim=1)
         return x
